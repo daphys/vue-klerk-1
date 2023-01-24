719a6f770035bb95414e3bdb93b8b286642adf46
joe <spunev1337@gmail.com>
2023-01-24 23:14:27 +0300

    1

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <p>123</p>
+  <tree />
+</template>
+
+<script>
+import Tree from "./components/Tree.vue";
+export default {
+  components: { Tree },
+};
+</script>
+
+<style scoped></style>
---
+++ src/components/Tree.vue
@@ -0,0 +1,30 @@
+<template>
+  <div v-for="(request, id) in requestInfo.data" :key="id">
+    {{ request }}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isAllowEmpty: false,
+      requestInfo: null,
+    };
+  },
+  mounted() {
+    this.axios
+      .get("https://www.klerk.ru/yindex.php/v3/event/rubrics")
+      .then((response) => {
+        try {
+          // сохраняем данные
+          this.requestInfo = response;
+        } catch (e) {
+          console.error("text error:", e);
+        }
+      });
+  },
+};
+</script>
+
+<style></style>
